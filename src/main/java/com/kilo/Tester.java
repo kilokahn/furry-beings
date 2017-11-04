061742f3a6e122b755b1e1dc7c3428fd45daf649
kilokahn <mukarram.baig@gmail.com>
2017-11-04 21:51:34 +0100

    nitial Commit

---
+++ src/main/java/com/kilo/Tester.java
@@ -0,0 +1,11 @@
+package com.kilo;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Tester {
+    private static final Logger LOG = LoggerFactory.getLogger(Tester.class);
+    public static void main(String args[]) {
+        LOG.info("Hello World!");
+    }
+}
---
+++ src/test/java/com/kilo/SomeTest.java
@@ -0,0 +1,15 @@
+package com.kilo;
+
+import org.junit.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class SomeTest {
+
+    private static final Logger LOG = LoggerFactory.getLogger(SomeTest.class);
+
+    @Test
+    public void test() {
+        LOG.info("Tested something correctly!");
+    }
+}
